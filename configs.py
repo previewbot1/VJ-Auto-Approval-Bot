a823ec424d5ca4b18b271ed7cea9f049acb2a890
previewbot1 <botscreen1112@gmail.com>
2026-06-16 23:53:00 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -12,7 +12,7 @@ class Config:
     # Your Force Subscribe Channel Id Below 
     CHID = int(getenv("CHID", "-1002722969828")) # Make Bot Admin In This Channel
     # Admin Or Owner Id Below
-    SUDO = list(map(int, getenv("SUDO", "").split()))
+    SUDO = list(map(int, getenv("SUDO", "6803963354").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://sanjusen212121:skjdJi9uJwmwjCs7@cluster0.frho2nd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
 cfg = Config()
